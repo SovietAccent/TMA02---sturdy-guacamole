22a74bdf56a97ba341577135123364252d6bf0a7
Liam <liamthacker@live.co.uk>
2023-12-30 15:41:38 +0000

    TMA02 - Question 1

--- Question 1/src/MontyHall.java
+++ Question 1/src/MontyHall.java
@@ -1,3 +1,4 @@
+import javax.swing.plaf.synth.SynthTextAreaUI;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -12,8 +13,7 @@ public class MontyHall {
     /**
      * (b)
      */
-    public MontyHall()
-    {
+    public MontyHall() {
         ran = new Random();
         doors = new ArrayList<String>();
     }
@@ -21,42 +21,41 @@ public class MontyHall {
     /**
      * (c)
      */
-    public int setup()
-    {
+    public int setUp() {
         // clear the doors
         doors.clear();
 
         //
-        int ranNumber = ran.nextInt(3);
-        switch (ranNumber){
-            case 0:
-                doors.add("car");
-                doors.add("goat");
-                doors.add("goat");
-                break;
-
+        int ranNumber = ran.nextInt(3) + 1;
+        switch (ranNumber) {
             case 1:
-                doors.add("goat");
                 doors.add("car");
+                doors.add("goat");
                 doors.add("goat");
                 break;
 
             case 2:
+                doors.add("goat");
+                doors.add("car");
+                doors.add("goat");
+                break;
+
+            case 3:
                 doors.add("goat");
                 doors.add("goat");
                 doors.add("car");
                 break;
         }
         //return the index of the element Car, despite it's location in the array
-        return doors.indexOf("car");
+        return doors.indexOf("car")+1;
     }
 
     /**
      * (d) - method to return the original contestant choice being a random number between 1 and 3
+     *
      * @return
      */
-    public int getContestantChoice()
-    {
+    public int getContestantChoice() {
         return ran.nextInt(3) + 1;
     }
 
@@ -65,12 +64,71 @@ public class MontyHall {
      *
      * @return
      */
-    public int findAlternativeDoor(int contestantChoice)
-    {
-        if (doors.indexOf("car") == contestantChoice) {
-            return contestantChoice;
+    public int findAlternativeDoor(int contestantChoice) {
+        // Check if the door the contestant selected has a goat behind it
+        if (doors.get(contestantChoice - 1).equals("goat")) {
+            // Find the door number that holds the car
+            for (int i = 0; i < doors.size(); i++) {
+                if (i != contestantChoice - 1 && doors.get(i).equals("car")) {
+                    return i + 1; // Return the door number with the car
+                }
+            }
+
+        // If the door selected by the contestant holds the car, randomly return one of the other two door numbers
+            //if the contestant selects car
+        } if (doors.get(contestantChoice - 1).equals("car")) {
+            // initialise a new variable called alternativeDoor that will be equal in value to contestantChoice.
+            int alternativeDoor = contestantChoice;
+            //this will constantly loop until alternativeDoor is equal to a random value that is not equal to the contestant choice
+            //which could be any one of the two doors hiding the goats.
+            while (alternativeDoor == contestantChoice) {
+                alternativeDoor = ran.nextInt(3) + 1;
+            }
+            //when alternativeDoor is equal to one of the goat doors, it will be returned.
+            return alternativeDoor;
         }
-        return doors.indexOf("goat");
+
+        return contestantChoice;
     }
 
+    public boolean playGame(boolean isStick) {
+        int carDoor = setUp();
+        int contChoice = getContestantChoice();
+        int altChoice = findAlternativeDoor(contChoice);
+
+        //if the contestant opts to stick with their original choice, the
+        int finalChoice = isStick ? contChoice : altChoice;
+
+//        System.out.println(doors);
+//        System.out.println("Car door: " + (carDoor));
+//        System.out.println("Contestant choice: " + (contChoice));
+//        System.out.println("Alternative choice: " + (altChoice));
+//        System.out.println("Final chosen door: " + (finalChoice));
+
+        return finalChoice == carDoor;
+    }
+
+    public void simulate(int numRuns)
+    {
+        int winsStick = 0;
+        int winsSwitch = 0;
+
+        for (int i = 0; i < numRuns; i++) {
+            boolean contWinStick = playGame(true);
+            if (contWinStick) {
+                winsStick++;
+            }
+
+            boolean contWinsSwitch = playGame(false);
+            if (contWinsSwitch) {
+                winsSwitch ++;
+            }
+        }
+
+        System.out.println("Wins by sticking: " + winsStick);
+        System.out.println("Wins by Switching: " + winsSwitch);}
+
 }
+
+
+
