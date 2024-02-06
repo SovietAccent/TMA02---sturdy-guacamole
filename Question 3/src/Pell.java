87ed78025e8e3d84c639d017c357d81750a27967
SovietAccent <liamthacker@live.co.uk>
2024-02-06 20:10:57 +0000

    Hullo

---
+++ Question 3/src/Pell.java
@@ -0,0 +1,26 @@
+public class Pell {
+
+    // (a)
+    private static final int COUNT = 10;
+    public int[] pellNums;
+
+    //(b)
+    public Pell()
+    {
+        pellNums = new int[COUNT];
+    }
+
+    //(c)
+    public int calculatePellNumAt(int n)
+    {
+
+    }
+
+    //(d)
+//    public double estimateRoot2(int n)
+//    {
+//
+//    }
+
+
+}
